f16347d8118d2dffb454c4f3c62a2a9b65ffaf15
Nyholm <tobias.nyholm@gmail.com>
2019-08-06 18:18:23 +0200

    cs

--- src/Exception/Domain/ClientException.php
+++ src/Exception/Domain/ClientException.php
@@ -5,8 +5,6 @@ namespace HappyrMatch\ApiClient\Exception\Domain;
 use HappyrMatch\ApiClient\Exception\DomainException;
 
 /**
- *
- *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
 class ClientException extends \Exception implements DomainException
