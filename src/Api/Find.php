d1f715547b95ad06e104ac1d2c303542e6dacc4f
Radoje Albijanic <radoje.albijanic@gmail.com>
2019-10-28 13:14:20 +0100

    Post parameter fix (#11)

--- src/Api/Find.php
+++ src/Api/Find.php
@@ -13,16 +13,16 @@ use Webmozart\Assert\Assert;
  */
 class Find extends HttpApi
 {
-    public function create(string $role, string $type, string $callbackUri)
+    public function create(string $role, string $type, string $callbackUrl)
     {
         Assert::notEmpty($role, 'Role cannot be empty');
         Assert::notEmpty($type, 'Type cannot be empty');
-        Assert::notEmpty($callbackUri, 'CallbackUri cannot be empty');
+        Assert::notEmpty($callbackUrl, 'CallbackUrl cannot be empty');
 
         $response = $this->httpPost('/api/find', [
             'role' => $role,
             'type' => $type,
-            'callback_uri' => $callbackUri,
+            'callback_url' => $callbackUrl,
         ]);
 
         if (!$this->hydrator) {
