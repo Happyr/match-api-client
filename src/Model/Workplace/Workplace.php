0c440d2f862ed5732d9d1b5dec4b2759cdf29a6f
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-04-13 21:25:02 +0200

    Added API support for Workplace (#18)

---
+++ src/Model/Workplace/Workplace.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HappyrMatch\ApiClient\Model\Workplace;
+
+final class Workplace
+{
+    private $id;
+    private $organization;
+    private $name;
+    private $country;
+    private $region;
+    private $city;
+    private $address;
+
+    public static function createFromArray(array $data)
+    {
+        $data = isset($data['data']) ? $data['data'] : $data;
+        $model = new self();
+        $model->id = $data['id'];
+        $model->organization = $data['attributes']['organization'];
+        $model->name = $data['attributes']['name'];
+        $model->country = $data['attributes']['country'];
+        $model->region = $data['attributes']['region'];
+        $model->city = $data['attributes']['city'];
+        $model->address = $data['attributes']['address'];
+
+        return $model;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getOrganization(): string
+    {
+        return $this->organization;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+}
